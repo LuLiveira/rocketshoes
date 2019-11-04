03f5b3ff95bf1ab4c2997b53bf385faa6efd54c9
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-11-04 19:08:32 -0300

    Estilizando a Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img src="" alt="" />
+        <strong>T</strong>
+        <span>120</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
